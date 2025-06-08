a4792cb03d1d1f42e063b283a7c14c6b7ae5665b
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2025-06-08 13:24:15 +0530

    Delete HMS Project crud operations/api-development directory

--- HMS Project crud operations/api-development/Hospital_management/src/main/java/com/example/project/Hospital_management/exception/PatientNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.Hospital_management.exception;
-
-public class PatientNotFoundException extends RuntimeException {
-    public PatientNotFoundException(String message) {
-        super(message);
-    }
-   
-}
